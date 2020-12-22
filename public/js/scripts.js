bf142eb4dee294477edea80522a2b79bb54cd119
RoyaShahroudi <royaShahroudi@github.com>
2020-12-22 17:10:30 +0330

    add read more btn

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,8 +9,20 @@ $(function() {
         } else  {
             $('.sticky-menu').removeClass('slide-in-top');
         }
-            
     });
+    
 
+    let textBox = $('.text-read-more');
+    let readMoreBtn = $('.read-more-btn');
+
+    $('.read-more-btn').click(function(){
+        textBox.toggle("fast");
+        if (readMoreBtn.html() === '<i class="fa fa-chevron-down"></i>') {
+            readMoreBtn.html('<i class="fa fa-chevron-up"></i>');
+        } else {
+            readMoreBtn.html('<i class="fa fa-chevron-down"></i>');
+        }
+    });
+   
 
 });
